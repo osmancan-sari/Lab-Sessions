8bfaf0dd13c4b41e4d821a55ccd2122f8f93fea8
Osmancan Sarı <92295909+itu-itis21-sario20@users.noreply.github.com>
2025-03-13 14:05:40 +0300

    Initial commit

---
+++ Lab_2/Lab2.3.1 - Dictionaries Creation.py
@@ -0,0 +1,19 @@
+"""
+YZV102E - Introduction to Programming for Data Science (Python)
+Lab 2: Store and Access Data
+
+Part 3: Dictionaries
+In this part, we will investigate the dictionaries in Python.
+"""
+
+lst1 = []
+lst2 = list()
+lst3 = [1, 2, 3]
+lst4 = ["itu", "1773"]
+lst5 = ["itu", 1773]
+
+print(lst1)
+print(lst2)
+print(lst3)
+print(lst4)
+print(lst5)
